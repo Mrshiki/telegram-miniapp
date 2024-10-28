e5f21fa73825a083176159c415c248139a08a90f
Денис Воробьёв <vorobiev@siberian.pro>
2024-10-28 19:41:23 +0700

    test miniapp

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tg = window.Telegram.WebApp;
+
+    tg.ready();
+
+    const user = tg.initDataUnsafe.user;
+    document.getElementById("user-info").innerText = `Hello, ${user.first_name}!`;
+
+    document.getElementById('notify-btn').addEventListener("click", function () {
+        tg.sendData(JSON.stringify({ message: 'User wants notifications!' }))
+    })
+})
---
+++ bot.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7836967736:AAHZdjkZART3H9OaT8vtY-Ht923Os87waL8';
+const bot = new TelegramBot(token, { polling: true });
+
+const webAppUrl = 'https://yourdomain.com/index.html';
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Открой миниапп:', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'Открыть миниапп', web_app: { url: webAppUrl } }]
+            ]
+        }
+    });
+});
+
+bot.on('web_app_data', (msg) => {
+    const data = JSON.parse(msg.web_app_data.data);
+    console.log('Получено от миниапа:', data);
+
+    // Ответ пользователю
+    bot.sendMessage(msg.chat.id, `Вы нажали кнопку!`);
+});
+
+
